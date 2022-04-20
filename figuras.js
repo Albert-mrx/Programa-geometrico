f7750c9a9944d3bb9847e696b29a6c83e697e99c
albert <dliktans@gmail.com>
2022-04-19 20:12:53 -0500

    aqui se agrego mas estilos y algunas modificasiones en parrafos P

--- figuras.js
+++ figuras.js
@@ -94,12 +94,64 @@ function circulo(radio){
 }
 circulo();
 */
+
+// codigo del teacher
+console.group("Cuadrados");
+function perimetroCuadrado(lado) {
+    return lado * 4;
+  }
+function areaCuadrado(lado) {
+    return lado * lado;
+  }
+console.groupEnd();
+//-----------------------------------------------------
+console.group("Triángulos");
+
+function perimetroTriangulo(lado1, lado2, base) {
+    return lado1 + lado2 + base;
+  }
+  function areaTriangulo(base, altura) {
+    return (base * altura) / 2;
+  }
+  console.log("El área del triángulo es: " + areaTriangulo + "cmˆ2");
+  
+  console.groupEnd();
+//---------------------------------------------------
+  console.group("Círculos");
+  function diametroCirculo(radio) {
+    return radio * 2;
+  }
+  
+  // PI
+  const PI = Math.PI;
+  console.log("PI es: " + PI);
+  
+  // Circunferencia
+  function perimetroCirculo(radio) {
+    const diametro = diametroCirculo(radio);
+    return diametro * PI;
+  }
+  
+  // Área
+  function areaCirculo(radio) {
+    return (radio * radio) * PI;
+  }
+  
+  console.groupEnd();
+  
+  
+
 //-----aqui interactuamos con el HTML ------
 
 function calcularPerimetroCuadrado(){
     const input=document.getElementById("Inputcuadrado");
     const Value=input.value;
+    const perimetro=perimetroCuadrado(Value);
+    alert(perimetro);
 }
 function calcularAreaCuadrado(){
-
+  const input=document.getElementById("Inputcuadrado");
+  const Value=input.value;
+  const area=areaCuadrado(Value);
+  alert(area);
 }
